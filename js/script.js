1b5b1f68b5845168212a86340de46a36877b9db7
Filipe Santos <143547860+Filipe-S-Silva@users.noreply.github.com>
2026-02-01 22:10:02 -0300

    Session storage index
    Add URL parameter handling and session storage for sender and receiver.

--- js/script.js
+++ js/script.js
@@ -2,8 +2,27 @@ const envelope = document.querySelector(".envelope");
 const letter = document.querySelector(".letter");
 const wrapper = document.querySelector(".wrapper");
 
+window.addEventListener('load', () => {
+    // Captura os parâmetros da URL
+    const urlParams = new URLSearchParams(window.location.search);
+    const sender = urlParams.get('sender');
+    const receiver = urlParams.get('receiver');
+
+    // Se existirem na URL, salva no localStorage e limpa a URL
+    if (sender && receiver) {
+        sessionStorage.setItem('cardSender', sender);
+        sessionStorage.setItem('cardReceiver', receiver);
+
+        // Remove os parâmetros da barra de endereço
+        const newUrl = window.location.origin + window.location.pathname;
+        window.history.replaceState({}, document.title, newUrl);
+    }
+
+   
+});
+
 envelope.addEventListener("click", () => {
-    const receiverParams = new URLSearchParams(window.location.search).get('receiver')
+    const receiverParams = sessionStorage.getItem('cardReceiver')
     const receiverElement = document.getElementById('receiver')
     
     receiverElement.textContent = `❤️ ${receiverParams.toLocaleUpperCase()} ❤️`
@@ -12,7 +31,6 @@ envelope.addEventListener("click", () => {
 });
 
 letter.addEventListener("click", () => {
-    const params = window.location.search;    
-    window.location.href= `../html/card.html${params}`
-
+    window.location.href= `../html/card.html`
 });
+
